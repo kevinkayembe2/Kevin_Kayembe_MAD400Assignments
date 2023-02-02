edeb68dfe3492034064fc2eb9cbb8e93cdd5dbc6
kevinkayembe2 <w0726803@myscc.ca>
2023-02-01 19:06:29 -0500

    Completed Step 10 of Assignment 1

--- src/app/models/content-list.ts
+++ src/app/models/content-list.ts
@@ -1,2 +1,21 @@
+import { IContent } from "./icontent";
+
 export class ContentList {
+    private _movies: IContent[];
+    constructor() {
+        this._movies = [];
+    }
+
+    get movies(): IContent[] {
+        return this._movies
+    }
+
+    addContent(newMovie: IContent) {
+        this._movies.push(newMovie);
+    }
+
+    arrayLength(): number {
+        return this._movies.length;
+    }
+
 }
