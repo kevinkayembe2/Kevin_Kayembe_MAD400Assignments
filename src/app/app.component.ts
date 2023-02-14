0c5a8749d7a3897ef1067a23b133f00110421eab
kevinkayembe2 <w0726803@myscc.ca>
2023-02-13 21:52:19 -0500

    Completed step 10 of Assignment 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { ContentList } from './models/content-list';
+import { Component, OnInit } from '@angular/core';
+import { IContent } from './models/icontent';
+import { MyFavouriteMovieService } from './services/my-favourite-movie.service';
 
 @Component({
   selector: 'app-root',
@@ -7,46 +8,8 @@ import { ContentList } from './models/content-list';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  singleMovie?: IContent;
   title = 'K_Kayembe_MyFavouriteMovies';
-  movieList: ContentList;
 
-  constructor() {
-    this.movieList = new ContentList();
-    this.movieList.addContent({
-      id: 0,
-      title: "Speed Racer",
-      description: "Futuristic Motorsport Racing",
-      author: "The Wachowskis",
-      imgSrc: "https://upload.wikimedia.org/wikipedia/en/8/82/Speed_racer_ver5_xlg.jpg",
-      type: "Racing",
-      tags: ["Speed Racer", "Racer X"]
-    });
-    this.movieList.addContent({
-      id: 1,
-      title: "Scott Pilgrim Vs. The World",
-      description: "A boy has to fight his girlfriend's 7 evil exes.",
-      author: "Edgar Wright",
-      imgSrc: "https://upload.wikimedia.org/wikipedia/en/1/14/Scott_Pilgrim_vs._the_World_teaser.jpg",
-      type: "Fighting",
-      tags: ["Scott Pilgrim", "Ramona Flowers", "Evil Exes"]
-    });
-    this.movieList.addContent({
-      id: 2,
-      title: "Sonic the Hedgehog",
-      description: "",
-      author: "Jeff Fowler",
-      imgSrc: "https://upload.wikimedia.org/wikipedia/en/4/45/Sonic_the_Hedgehog_film_poster.jpg",
-      type: "Adventure",
-      tags: ["Sonic", "Sonic (2020)", "Dr. Eggman"]
-    });
-    this.movieList.addContent({
-      id: 3,
-      title: "Paul Blart: Mall Cop",
-      description: "Three crooks, two donuts, one bumbling mall guard.",
-      author: "Steve Carr",
-      imgSrc: "https://upload.wikimedia.org/wikipedia/en/9/95/Paul_blart_mall_cop_film.jpg",
-      type: "Comedy",
-      tags: ["Paul Blart", "Scooter Patrol"]
-    });
-  }
+  constructor(private movieService: MyFavouriteMovieService) {}
 }
