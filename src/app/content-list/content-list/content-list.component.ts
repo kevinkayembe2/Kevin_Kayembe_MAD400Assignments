6e606c398c3b21d286f78c2294e0945c283bc2d6
kevinkayembe2 <w0726803@myscc.ca>
2023-02-13 20:15:48 -0500

    Completed step 7 of Assignment 3

--- src/app/content-list/content-list/content-list.component.ts
+++ src/app/content-list/content-list/content-list.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IContent } from 'src/app/models/icontent';
+import { MyFavouriteMovieService } from 'src/app/services/my-favourite-movie.service';
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.scss']
 })
-export class ContentListComponent {
+export class ContentListComponent implements OnInit {
   movieArray: IContent[];
 
-  constructor() {
+  constructor(private movieSerivce: MyFavouriteMovieService) {
     this.movieArray = [];
+  }
+
+  ngOnInit(): void {
+    this.movieSerivce.getContent();
   }
 }
