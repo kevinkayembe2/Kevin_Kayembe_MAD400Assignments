140c9e9b2b2c6d09190bb1eefc25792f53f090ed
kevinkayembe2 <w0726803@myscc.ca>
2023-04-03 23:58:10 -0400

    Completed step 10 of Assignment 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { IContent } from './models/icontent';
-import { MyFavouriteMovieService } from './services/my-favourite-movie.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  singleMovie?: IContent;
+export class AppComponent {
   title = 'K_Kayembe_MyFavouriteMovies';
-
-  constructor(private movieService: MyFavouriteMovieService) {}
-  
-  ngOnInit(): void {
-    this.getTheNewItem("3");
-  }
-
-  getTheNewItem(newIdNumber: string): void {
-    console.warn("Getting an item: ", newIdNumber);
-    this.movieService.getContentItem(Number(newIdNumber)).subscribe((movie: IContent) => {
-      console.warn("Got the item: ", movie);
-      this.singleMovie = movie;
-    });
-  }
 }
--- src/app/content-list-item-search/content-list-item-search.component.ts
+++ src/app/content-list-item-search/content-list-item-search.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IContent } from '../models/icontent';
+import { MyFavouriteMovieService } from '../services/my-favourite-movie.service';
 
 @Component({
   selector: 'app-content-list-item-search',
   templateUrl: './content-list-item-search.component.html',
   styleUrls: ['./content-list-item-search.component.scss']
 })
-export class ContentListItemSearchComponent {
+export class ContentListItemSearchComponent implements OnInit {
+  singleMovie?: IContent;
 
+  constructor(private movieService: MyFavouriteMovieService) {}
+  
+  ngOnInit(): void {
+    this.movieService.getContentItem(3).subscribe((movie: IContent) => {
+      console.warn("Got the item: ", movie);
+      this.singleMovie = movie;
+    });
+  }
 }
