48055f9e1f154e70e5deb3c6b0678bae4e4d3bf6
kevinkayembe2 <w0726803@myscc.ca>
2023-04-04 00:27:03 -0400

    Completed bullet point 1 of step 14 of Assignment 4

--- src/app/content-list-item-detail/content-list-item-detail.component.ts
+++ src/app/content-list-item-detail/content-list-item-detail.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IContent } from '../models/icontent';
+import { MyFavouriteMovieService } from '../services/my-favourite-movie.service';
 
 @Component({
   selector: 'app-content-list-item-detail',
   templateUrl: './content-list-item-detail.component.html',
   styleUrls: ['./content-list-item-detail.component.scss']
 })
-export class ContentListItemDetailComponent {
+export class ContentListItemDetailComponent implements OnInit {
+  id?: number;
+  movieDetailItem?: IContent;
 
+  constructor(private route: ActivatedRoute,
+    private movieService: MyFavouriteMovieService) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.id = Number(params.get('idNumberValue'));
+
+      this.movieService.getContentItem(this.id).subscribe((movie: IContent) => {
+        console.warn("Got the item in the detail: ", movie);
+         this.movieDetailItem = movie;
+      });
+
+    });
+  }
 }
