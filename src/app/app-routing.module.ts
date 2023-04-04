d5129df9af5197c238437eb732eaf73ee4bb39f2
kevinkayembe2 <w0726803@myscc.ca>
2023-04-03 23:36:03 -0400

    Completed first bullet point of step 7 of Assignment 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ContentListComponent } from './content-list/content-list/content-list.component';
 
-
+const routes: Routes = [
+  {
+    path: "content",
+    component: ContentListComponent
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list/content-list.component';
 import { ContentListItemComponent } from './content-list-item/content-list-item/content-list-item.component';
 import { ContentListItemDetailComponent } from './content-list-item-detail/content-list-item-detail.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ContentListItemDetailComponent } from './content-list-item-detail/conte
     ContentListItemDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
