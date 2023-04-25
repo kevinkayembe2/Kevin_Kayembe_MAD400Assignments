237e96afcbf30a062ae7ee4910c0887dce8999a4
kevinkayembe2 <w0726803@myscc.ca>
2023-04-25 16:46:48 -0400

    Completed step 15 of Assignment 5

--- src/app/modify-content/modify-content.component.ts
+++ src/app/modify-content/modify-content.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IContent } from '../models/icontent';
 import { INVALIDMOVIE } from '../data/mock-content';
+import { MyFavouriteMovieService } from '../services/my-favourite-movie.service';
 
 @Component({
   selector: 'app-modify-content',
@@ -10,4 +11,26 @@ import { INVALIDMOVIE } from '../data/mock-content';
 export class ModifyContentComponent {
   newContentItem: IContent = INVALIDMOVIE;
   tagsToBeParsed: string = "";
+  contentAddedSuccessfully = false;
+
+  constructor(private movieService: MyFavouriteMovieService) {
+    this.resetContent();
+  }
+
+  addContent(): void {
+    this.newContentItem.tags = this.tagsToBeParsed.split(",").map(tag => tag.trim()).filter(tag => tag.length > 0);
+    this.newContentItem.id = undefined;
+    this.movieService.addContentItem(this.newContentItem).subscribe(newItemFromServer => {
+      console.log("Movie Successfully Added", newItemFromServer);
+
+      this.contentAddedSuccessfully = true;
+
+      this.resetContent();
+    });
+  }
+
+  resetContent(): void {
+    this.newContentItem = structuredClone(INVALIDMOVIE);
+    this.tagsToBeParsed = "";
+  }
 }
